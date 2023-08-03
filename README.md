fcd1f7292fbecdadf0db80e662f1cfd3049f50b1
pipe <felipe.parra@blackmind.cl>
2023-08-03 14:04:34 -0400

    last commit

---
+++ README.md
@@ -0,0 +1,118 @@
+<img src="public/images/boldoMedio.png" align="right" />
+ <h1 align= left ><b>Grupal 1 M7 /Equipo Boldo</b> <img src = "https://media.giphy.com/media/gF2m2JOyGReppog8hU/giphy.gif" width = 80px></h1>
+
+<br>
+
+<h2><b>Descripción del Proyecto</b> <img src = " https://media.giphy.com/media/GjhqaB166nKR4BoEnh/giphy.gif" width = 50px></h2>
+
+📝 La escuela de música Always Music es reconocida en la ciudad por graduar a grandes músicos de reconocimiento
+mundial, sin embargo, a pesar de lo mucho que ha crecido con el tiempo, nunca dejaron de utilizar excel como base
+de datos y han decidido cambiar esto por un desarrollo personalizado.
+En este desafío deberás desarrollar una aplicación en Node que realice consultas a Base de Datos (usada en el
+correspondiente Reading) con el paquete para:
+ Agregar un nuevo estudiante.
+ Consultar los estudiantes registrados.
+ Consultar estudiante por rut.
+ Actualizar la información de un estudiante.
+ Eliminar el registro de un estudiante.
+Ya que el caso se trata de un proceso de desarrollo, la interacción la debes realizar con argumentos por la línea de
+comandos.
+Antes de iniciar este desafío deberás crear una base de datos y una tabla con las siguientes columnas:
+ Nombre
+ Rut
+ Curso
+ Nivel
+
+<!-- ![Foto de grupo](public/images/example.png) -->
+
+1. Realizar la conexión a base de datos con la clase Client.
+2. Crear una función asíncrona para registrar un nuevo estudiante en la base de datos.
+3. Crear una función asíncrona para obtener por consola el registro de un estudiante por medio de su rut.
+4. Crear una función asíncrona para obtener por consola todos los estudiantes registrados.
+5. Crear una función asíncrona para actualizar los datos de un estudiante en la base de datos.
+6. Crear una función asíncrona para eliminar el registro de un estudiante de la base de datos.
+
+<br>
+
+<h2><b>Instrucciones de Instalación</b> <img src = "https://media.giphy.com/media/3WZJkScSyfYVl7mGLd/giphy.gif" width = 60px></h2> 
+
+⚙️ Para instalar las dependencias necesarias, sigue los siguientes pasos:
+
+1. Clona el repositorio en tu máquina local.
+2. Abre una terminal y navega hasta la carpeta del proyecto.
+3. Ejecuta el siguiente comando para instalar las dependencias:
+
+   npm install
+
+4. Abrir el archivo html ingresando al https://localhost:3000
+5. Para insertar colocar en la consola:  node insertar.js 04 'prueba1' '44444444-4' '4' '4'
+
+<br>
+
+<details> <img src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTFtdWgyMmFrcHd4NjhuZWJ4aDJpcTlkbWlyNGQ4dDJwa2ZwZmptcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/B4AgroOi1LkdPxMllY/giphy.gif" width = 50px> <summary><b>Ver las instrucciones</b></summary> 
+
+
+1. Instalar las dependencias:
+
+   ```sh
+   npm install
+   ```
+
+2. En el caso de no poder instalar las dependencias:
+
+   ```sh
+   npm install --force
+   ```
+
+3. Las librerias que estamos ocupando `package.json`:
+
+    ````sh
+    ... 
+    "name": "helpers",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    + "type": "module",
+    "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+    "express": "^4.18.2",
+    "pg": "^8.11.2"
+    }
+    ````
+
+ 
+</details>
+
+## Funcionalidades :sparkles:
+
+✨ Con nuestra aplicacion puedes:
+
+1. Funcionalidad : Agregar personas(roommates) de forma aleatoria
+2. Funcionalidad : Agregar Gastos y Descripcion de los mismos y guardarlos en un archivo .json
+3. Funcionalidad : Calcular los que deben y lo que reciben cada uno de los roommates 
+
+
+<!-- ![Foto de grupo](public/images/grupoVerde.jpg) -->
+
+## Participantes del Grupo :busts_in_silhouette:
+
+1. Loreto Godoy : https://github.com/loreGodoyUp
+
+2. Daniel Mendez : https://github.com/danimen81
+
+3. Zimram Blanco : https://github.com/Zimram
+
+4. Jose Fuentealba : https://github.com/JoseFelipeGeo
+
+5. Boris Guiñez : https://github.com/boanguibe
+
+6. Felipe Andres Parra : https://github.com/33fparra
+
+
+### Este proyecto está bajo la licencia ADALID.
+<img src="public/images/boldoMedio.png" align="right" />
+
--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-// La escuela de música Always Music es reconocida en la ciudad por graduar a grandes músicos de reconocimiento
-// mundial, sin embargo, a pesar de lo mucho que ha crecido con el tiempo, nunca dejaron de utilizar excel como base
-// de datos y han decidido cambiar esto por un desarrollo personalizado.
-// En este desafío deberás desarrollar una aplicación en Node que realice consultas a Base de Datos (usada en el
-// correspondiente Reading) con el paquete para:
-//  Agregar un nuevo estudiante.
-//  Consultar los estudiantes registrados.
-//  Consultar estudiante por rut.
-//  Actualizar la información de un estudiante.
-//  Eliminar el registro de un estudiante.
-// Ya que el caso se trata de un proceso de desarrollo, la interacción la debes realizar con argumentos por la línea de
-// comandos.
-// Antes de iniciar este desafío deberás crear una base de datos y una tabla con las siguientes columnas:
-//  Nombre
-//  Rut
-//  Curso
-//  Nivel
-// Usa las siguientes imágenes como referencia de lo que debes lograr en el desarrollo de este desafío.
+import pg  from 'pg';
 
-import pg from 'pg';
 
 const client = new pg.Client({
     host: 'localhost',
@@ -27,15 +9,111 @@ const client = new pg.Client({
     port: 5432
 });
 
-client.connect();
+async function conectar() {
+  try {
+    await client.connect();
+    console.log('Conexión a la base de datos establecida');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos', error);
+  }
+}
 
-client.query("SELECT * FROM estudiantes", (err, res) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(res.rows);
+// Función para agregar un nuevo estudiante
+async function agregarEstudiante(id, nombre, rut, curso, nivel) {
+    try {
+      const query = `
+        INSERT INTO estudiantes (id, nombre, rut, curso, nivel)
+        VALUES ($1, $2, $3, $4, $5)
+      `;
+      const values = [id, nombre, rut, curso, nivel];
+      await client.query(query, values);
+      console.log('Estudiante', nombre, 'Agregado con éxito');
+    } catch (error) {
+      console.error('Error al agregar el estudiante', error);
     }
-    client.end(); // Cerrar la conexión después de la consulta
-});
+  } //esta funcion esta bien
+  
+  // Función para consultar todos los estudiantes
+  async function consultarEstudiantes() {
+    try {
+      const result = await client.query('SELECT * FROM estudiantes');
+      console.log('Registro actual', result.rows);
+    } catch (error) {
+      console.error('Error al consultar los estudiantes', error);
+    }
+  }   //esta funcion esta bien
+  
+  // Función para consultar un estudiante por rut
+  async function consultarEstudiantePorRut(rut) {
+    try {
+      const query = 'SELECT * FROM estudiantes WHERE rut = $1';
+      const result = await client.query(query, [rut]);
+      console.log(result.rows);
+    } catch (error) {
+      console.error('Error al consultar el estudiante', error);
+    }
+  }
+  
+  // Función para editar los datos de un estudiante
+  async function editarEstudiante(id, nombre, rut, curso, nivel) {
+    try {
+      const query = `
+        UPDATE estudiantes
+        SET nombre = $2, curso = $3, nivel = $4
+        WHERE rut = $1
+      `;
+      const values = [rut, nombre, curso, nivel];
+      await client.query(query, values);
+      console.log('Estudiante', nombre, 'editado con éxito');
+    } catch (error) {
+      console.error('Error al editar el estudiante', error);
+    }
+  }
+  
+  // Función para eliminar un estudiante por rut
+  async function eliminarEstudiantePorRut(rut) {
+    try {
+      const query = 'DELETE FROM estudiantes WHERE rut = $1';
+      await client.query(query, [rut]);
+      console.log('Registro de estudiante con rut', rut, 'eliminado');
+    } catch (error) {
+      console.error('Error al eliminar el estudiante', error);
+    }
+  }
+  
+  // Aca manejamos los comandos de la línea de comandos
+async function main() {
+  await conectar();
 
+  const command = process.argv[2];
 
+  switch (command) {
+    case 'nuevo':
+      const nuevoArgs = process.argv.slice(3);
+      await agregarEstudiante(...nuevoArgs);
+      break;
+    case 'consulta':
+      await consultarEstudiantes();
+      break;
+    case 'editar':
+      const editarArgs = process.argv.slice(3);
+      await editarEstudiante(...editarArgs);
+      break;
+    case 'rut':
+      const rutArg = process.argv[3];
+      await consultarEstudiantePorRut(rutArg);
+      break;
+    case 'eliminar':
+      const eliminarArg = process.argv[3];
+      await eliminarEstudiantePorRut(eliminarArg);
+      break;
+    default:
+      console.log('Comando no reconocido');
+      break;
+  }
+
+  await client.end();
+}
+
+// Ejecutar la aplicación
+main().catch((error) => console.error('Error en la aplicación', error));
